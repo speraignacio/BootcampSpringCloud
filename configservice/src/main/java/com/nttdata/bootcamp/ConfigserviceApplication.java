d1c0950d3d1e1a4f8d13ddc9c4c1abef860c51ca
speraignacio <speraignacio@gmail.com>
2022-02-09 09:36:47 +0100

    first commit

---
+++ configservice/src/main/java/com/nttdata/bootcamp/ConfigserviceApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserviceApplication.class, args);
+	}
+
+}
